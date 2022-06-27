2446d619f6fcf5fba6280113720d172f66d1de67
Nick Semenyuk <semka147semov@gmail.com>
2022-06-27 14:28:51 +0300

    Bug fixes and other minor improvements
    - FIxed bug with margins when there is no posts
    - Added info about project to home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,11 @@ const Home = () => {
     return (
         <>
             <div className='Stories-logo'>
-            <h1 className='Stories-logo_title'>Grass Stories</h1>
-            <p className='Stories-logo_subtitle'>*da best grass Stories ever</p>
-        </div>
+                <h1 className='Stories-logo_title'>Grass Stories</h1>
+                <p className='Stories-logo_subtitle'>*da best grass Stories ever</p>
+                <br/>
+                <p className='Stories-info'> This project was made for my own purposes to practice with React-Router and search method, also helped me alot to make footer and navigation between pages.</p>
+            </div>
         </>
      );
 }
